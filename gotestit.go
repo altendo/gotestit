e8b692d9de2abc54e6716b75c4cce2f927af880b
Nick Antonelli <nick.antonelli@shadowhealth.com>
2016-03-17 16:52:45 -0400

    open all config files in a directory

--- gotestit.go
+++ gotestit.go
@@ -6,6 +6,7 @@ import(
     "log"
     "time"
     "os/exec"
+    "path/filepath"
     "encoding/json"
 
     "github.com/robfig/cron"
@@ -16,7 +17,7 @@ var c *cron.Cron
 type ExecutableJob struct {
     Name        string `json:"name"`
     CronExp     string `json:"cronexp"`
-    Script      string `json:"script"`
+    Cmd         string `json:"cmd"`
     Args        string `json:"args"`
     StdoutPath  string `json:"stdoutPath"`
     StderrPath  string `json:"stderrPath"`
@@ -27,7 +28,7 @@ type ExecutableJob struct {
 
 func (ej *ExecutableJob) Run() {
 
-    cmd := exec.Command(ej.Script, ej.Args)
+    cmd := exec.Command(ej.Cmd, ej.Args)
 
     cmd.Stdout = ej.Stdout
     cmd.Stderr = ej.Stderr
@@ -40,16 +41,17 @@ func (ej *ExecutableJob) Run() {
 
 }
 
-func main() {
+/*
+ * In the future, we may want to open configs in separate goroutines, but
+ * keep it serialized for now (version 2 or 3 feature?)
+ */
+func openJobConfig(path string) *ExecutableJob {
 
-    c = cron.New()
-
-    // open the file
-    configPath := "test.json"
-    file, err := os.Open(configPath)
+    file, err := os.Open(path)
+    defer file.Close()
 
     if err != nil {
-        log.Fatal("error opening config file,", err)
+        log.Fatal("error opening job config file: ", err)
     }
 
     // parse the config
@@ -58,27 +60,53 @@ func main() {
 
     err = decoder.Decode(&ej)
     if err != nil {
-        log.Fatal("error decoding job information,", err)
+        log.Fatal("error decoding job information: ", err)
     }
 
+    openFileFlags := os.O_RDWR|os.O_APPEND|os.O_CREATE
+    openFilePerms := os.FileMode(0644)
+
     // change the job to write out to custom stderr/stdout
-    if outFile, err := os.OpenFile(ej.StdoutPath, os.O_RDWR|os.O_APPEND|os.O_CREATE, 0644); err != nil {
+    if outFile, err := os.OpenFile(ej.StdoutPath, openFileFlags, openFilePerms); err != nil {
         log.Fatal("unable to open stdout for writing: ", err)
     } else {
         ej.Stdout = outFile
     }
 
-    if errFile, err := os.OpenFile(ej.StderrPath, os.O_RDWR|os.O_APPEND|os.O_CREATE, 0644); err != nil {
+    if errFile, err := os.OpenFile(ej.StderrPath, openFileFlags, openFilePerms); err != nil {
         log.Fatal("unable to open stderr for writing: ", err)
     } else {
         ej.Stderr = errFile
     }
 
-    // add the job to the queue
-    c.AddJob(ej.CronExp, ej)
-    c.Start()
+    return ej
 
+}
+
+func main() {
+
+    c = cron.New()
     defer c.Stop()
+
+    configDirPath := "/home/nickantonelli/gotestit.d"
+    // open the file
+    dir, err := os.Open(configDirPath)
+    if err != nil {
+        log.Fatal("unable to open config directory for parsing: ", err)
+    }
+
+    filenames, err := dir.Readdirnames(0)
+    if err != nil {
+        log.Fatal("unable to get all file names: ", err)
+    }
+
+    for _, filename := range filenames {
+        // add the job to the queue
+        job := openJobConfig(filepath.Join(configDirPath, filename))
+        c.AddJob(job.CronExp, job)
+    }
+
+    c.Start()
 
     // let it ride
     for {
