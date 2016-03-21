679df7e73436cc4dea992725bb9d78c761021dd1
Nick Antonelli <nick.antonelli@shadowhealth.com>
2016-03-21 12:58:26 -0400

    run gofmt prior to v1 release

--- gotestit.go
+++ gotestit.go
@@ -1,18 +1,18 @@
 package main
 
-import(
-    "os"
-    "io"
-    "log"
-    "fmt"
-    "time"
-    "flag"
-    "strings"
-    "os/exec"
-    "path/filepath"
-    "encoding/json"
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+	"time"
 
-    "github.com/robfig/cron"
+	"github.com/robfig/cron"
 )
 
 var c *cron.Cron
@@ -21,55 +21,55 @@ var eventStream chan *CronEvent
 const DefaultConfigDirPath = "/etc/gotestit.d"
 
 type CronEvent struct {
-    EventJob        string
-    EventTime       time.Time
-    EventStatus     string
-    EventMessage    string
+	EventJob     string
+	EventTime    time.Time
+	EventStatus  string
+	EventMessage string
 }
 
 type ExecutableJob struct {
-    Name        string `json:"name"`
-    CronExp     string `json:"cronexp"`
-    Cmd         string `json:"cmd"`
-    Args        string `json:"args"`
-    StdoutPath  string `json:"stdoutPath"`
-    StderrPath  string `json:"stderrPath"`
+	Name       string `json:"name"`
+	CronExp    string `json:"cronexp"`
+	Cmd        string `json:"cmd"`
+	Args       string `json:"args"`
+	StdoutPath string `json:"stdoutPath"`
+	StderrPath string `json:"stderrPath"`
 
-    Stdout      io.Writer `json:"-"`
-    Stderr      io.Writer `json:"-"`
+	Stdout io.Writer `json:"-"`
+	Stderr io.Writer `json:"-"`
 }
 
 func (ej *ExecutableJob) Run() {
 
-    cmd := exec.Command(ej.Cmd, ej.Args)
+	cmd := exec.Command(ej.Cmd, ej.Args)
 
-    cmd.Stdout = ej.Stdout
-    cmd.Stderr = ej.Stderr
+	cmd.Stdout = ej.Stdout
+	cmd.Stderr = ej.Stderr
 
-    err := cmd.Run()
+	err := cmd.Run()
 
-    event := &CronEvent{
-        EventJob: ej.Name,
-        EventTime: time.Now(),
-        EventStatus: "success",
-        EventMessage: "",
-    }
+	event := &CronEvent{
+		EventJob:     ej.Name,
+		EventTime:    time.Now(),
+		EventStatus:  "success",
+		EventMessage: "",
+	}
 
-    if err != nil {
-        event.EventStatus = "failure"
-        event.EventMessage = fmt.Sprintf("error executing %s: %s", ej.Name, err)
-    } else {
-        event.EventMessage = fmt.Sprintf("successfully ran %s", ej.Name)
-    }
+	if err != nil {
+		event.EventStatus = "failure"
+		event.EventMessage = fmt.Sprintf("error executing %s: %s", ej.Name, err)
+	} else {
+		event.EventMessage = fmt.Sprintf("successfully ran %s", ej.Name)
+	}
 
-    eventStream <-event
+	eventStream <- event
 
 }
 
-
 var cmdFlagConfigDirPath string
+
 func init() {
-    flag.StringVar(&cmdFlagConfigDirPath, "d", DefaultConfigDirPath, "sets the directory in which to read job configurations")
+	flag.StringVar(&cmdFlagConfigDirPath, "d", DefaultConfigDirPath, "sets the directory in which to read job configurations")
 }
 
 /*
@@ -78,106 +78,105 @@ func init() {
  */
 func openJobConfig(path string) (*ExecutableJob, error) {
 
-    file, err := os.Open(path)
-    defer file.Close()
+	file, err := os.Open(path)
+	defer file.Close()
 
-    if err != nil {
-        log.Fatal("error opening job config file: ", err)
-    }
+	if err != nil {
+		log.Fatal("error opening job config file: ", err)
+	}
 
-    // parse the config
-    var ej *ExecutableJob
-    decoder := json.NewDecoder(file)
+	// parse the config
+	var ej *ExecutableJob
+	decoder := json.NewDecoder(file)
 
-    err = decoder.Decode(&ej)
-    if err != nil {
-        return nil, err
-    }
+	err = decoder.Decode(&ej)
+	if err != nil {
+		return nil, err
+	}
 
-    openFileFlags := os.O_RDWR|os.O_APPEND|os.O_CREATE
-    openFilePerms := os.FileMode(0644)
+	openFileFlags := os.O_RDWR | os.O_APPEND | os.O_CREATE
+	openFilePerms := os.FileMode(0644)
 
-    // change the job to write out to custom stderr/stdout
-    if outFile, err := os.OpenFile(ej.StdoutPath, openFileFlags, openFilePerms); err != nil {
-        log.Println("unable to open stdout for writing: ", err)
-    } else {
-        ej.Stdout = outFile
-    }
+	// change the job to write out to custom stderr/stdout
+	if outFile, err := os.OpenFile(ej.StdoutPath, openFileFlags, openFilePerms); err != nil {
+		log.Println("unable to open stdout for writing: ", err)
+	} else {
+		ej.Stdout = outFile
+	}
 
-    if errFile, err := os.OpenFile(ej.StderrPath, openFileFlags, openFilePerms); err != nil {
-        log.Println("unable to open stderr for writing: ", err)
-    } else {
-        ej.Stderr = errFile
-    }
+	if errFile, err := os.OpenFile(ej.StderrPath, openFileFlags, openFilePerms); err != nil {
+		log.Println("unable to open stderr for writing: ", err)
+	} else {
+		ej.Stderr = errFile
+	}
 
-    return ej, nil
+	return ej, nil
 
 }
 
 func main() {
 
-    flag.Parse()
+	flag.Parse()
 
-    c = cron.New()
-    defer c.Stop()
-    eventStream = make(chan *CronEvent)
+	c = cron.New()
+	defer c.Stop()
+	eventStream = make(chan *CronEvent)
 
-    // open the file
-    dir, err := os.Open(cmdFlagConfigDir)
-    if err != nil {
-        log.Fatal("unable to open config directory for parsing: ", err)
-    }
+	// open the file
+	dir, err := os.Open(cmdFlagConfigDir)
+	if err != nil {
+		log.Fatal("unable to open config directory for parsing: ", err)
+	}
 
-    if stat, err := dir.Stat(); err != nil {
-        log.Fatal("unable to stat " + cmdFlagConfigDir + ", error: ", err)
-    } else if !stat.IsDir() {
-        log.Fatal(cmdFlagConfigDir + " is not a valid directory")
-    }
+	if stat, err := dir.Stat(); err != nil {
+		log.Fatal("unable to stat "+cmdFlagConfigDir+", error: ", err)
+	} else if !stat.IsDir() {
+		log.Fatal(cmdFlagConfigDir + " is not a valid directory")
+	}
 
-    filenames, err := dir.Readdirnames(0)
-    if err != nil {
-        log.Fatal("unable to get all file names: ", err)
-    }
+	filenames, err := dir.Readdirnames(0)
+	if err != nil {
+		log.Fatal("unable to get all file names: ", err)
+	}
 
-    jobsAdded := false
+	jobsAdded := false
 
-    for _, filename := range filenames {
-        // add the job to the queue
-        jobConfig := filepath.Join(cmdFlagConfigDir, filename)
+	for _, filename := range filenames {
+		// add the job to the queue
+		jobConfig := filepath.Join(cmdFlagConfigDir, filename)
 
-        if hasSuffix := strings.HasSuffix(jobConfig, "json"); !hasSuffix {
-            continue
-        }
+		if hasSuffix := strings.HasSuffix(jobConfig, "json"); !hasSuffix {
+			continue
+		}
 
-        if job, err := openJobConfig(jobConfig); err !=  nil {
-            log.Printf("error decoding job information for %s: %s", jobConfig, err)
-        } else {
-            c.AddJob(job.CronExp, job)
-            jobsAdded = true
-        }
-    }
+		if job, err := openJobConfig(jobConfig); err != nil {
+			log.Printf("error decoding job information for %s: %s", jobConfig, err)
+		} else {
+			c.AddJob(job.CronExp, job)
+			jobsAdded = true
+		}
+	}
 
-    if !jobsAdded {
-        log.Fatal("no jobs found, exiting")
-    }
+	if !jobsAdded {
+		log.Fatal("no jobs found, exiting")
+	}
 
-    c.Start()
+	c.Start()
 
-    // let it ride
-    for {
-        select {
-        case event, ok := <-eventStream:
-            if !ok {
-                // not sure how we got here, maybe we caught a signal
-                break
-            }
-            if event.EventStatus == "success" {
-                log.Printf("ran job `%s` with message: %s\n", event.EventJob, event.EventMessage)
-            } else {
-                log.Printf("job `%s` failed: %s\n", event.EventJob, event.EventMessage)
-            }
-        default:
-        }
-    }
+	// let it ride
+	for {
+		select {
+		case event, ok := <-eventStream:
+			if !ok {
+				// not sure how we got here, maybe we caught a signal
+				break
+			}
+			if event.EventStatus == "success" {
+				log.Printf("ran job `%s` with message: %s\n", event.EventJob, event.EventMessage)
+			} else {
+				log.Printf("job `%s` failed: %s\n", event.EventJob, event.EventMessage)
+			}
+		default:
+		}
+	}
 }
-
